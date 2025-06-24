7c0dbe7adbafa9865be293fb06fc7793b68a4de6
Gustavo Almeida Zuchini <gustavo.zuchini@etec.sp.gov.br>
2025-06-23 22:49:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,6 @@ if (isset($_GET['busca']) && !empty(trim($_GET['busca']))) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Alexandria Biblioteca</title>
     <link rel="icon" href="/img/icons/LogoMiniaturaClaro.png" type="image/png">
-    <link rel="shortcut icon" href="/img/icons/LogoMiniaturaClaro.png" type="image/png">
 
 </head>
 
